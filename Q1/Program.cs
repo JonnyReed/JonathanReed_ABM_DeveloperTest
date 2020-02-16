8d501f51818b64a4336f24330bd46bf6c6d407ac
Jonathan Reed <jonnyreed345@hotmail.com>
2020-02-16 01:18:44 +0000

    Questions 1 and 2

---
+++ Q1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Q1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //read in all the lines from the document
+            string[] lines = File.ReadAllLines(@"data.edi");
+            List<string> locLines = new List<string>();
+            List<string> finalResults = new List<string>();
+
+            //extract all lines that have the text "LOC" in it
+            foreach (string line in lines)
+            {
+                if (line.Contains("LOC"))
+                    locLines.Add(line);
+            }
+
+            //separate each segment of the line containing "LOC"
+            foreach (string line in locLines)
+            {
+                string[] segments = line.Split('+');
+                for (int i = 0; i < segments.Length; i++)
+                {
+                    //filter out all of the 1st segments, leaving only the remaining 2 segments
+                    if (segments[i] != "LOC")
+                        finalResults.Add(segments[i]);
+                }
+            }
+
+            //print out the results
+            foreach (string result in finalResults)
+            {
+                Console.WriteLine(result);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Q2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //read in the document, and query it using Linq
+            IEnumerable<string> results = from data in XDocument.Load(@"data.xml")
+                                        .Descendants("DeclarationHeader")
+                                        .Descendants("Reference")//scope in on the desired element
+                                          where (string)data.Attribute("RefCode") == "MWB" ||
+                                           (string)data.Attribute("RefCode") == "TRV" ||
+                                           (string)data.Attribute("RefCode") == "CAR" //filter the results to match the requirements
+                                          select data.Element("RefText").Value; //extract the desired values
+
+            //print the results
+            foreach (string result in results)
+                Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
